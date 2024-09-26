28cbfbcb40650f8164279596a2a07f397f17a987
ThoNT16 <truongthonguyen107@gmail.com>
2024-09-26 22:03:47 +0700

    feat: add register component [ThoNT]

--- mentor-booking-web-app/src/utils/apiUrl/baseUrl.ts
+++ mentor-booking-web-app/src/utils/apiUrl/baseUrl.ts
@@ -4,3 +4,6 @@ export const BASE_URL = "https://localhost:7554/api";
 // API URL
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
 export const LOGOUT_URL = `${BASE_URL}/auth/logout`;
+export const STUDENT_REGISTER = `${BASE_URL}/User/student/sign-up`
+export const MENTOR_REGISTER = `${BASE_URL}/User/mentor/sign-up`
+
