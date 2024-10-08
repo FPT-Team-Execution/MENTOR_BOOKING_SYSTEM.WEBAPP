bfde947d25f9a0a4ed78dd662859d9b734df22f7
ThoNT16 <truongthonguyen107@gmail.com>
2024-10-08 20:53:03 +0700

    feat: add student management table UI [ThoNT]

---
+++ mentor-booking-web-app/src/pages/user/student/StudentPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import StudentTable from '../../../components/user/student/StudentTable'
+
+export const StudentPage = () => {
+  return (
+    <div className='w-full'>
+        <h1 className='text-lg font-bold'>Feedback Management</h1>
+        <StudentTable/>
+    </div>
+  )
+}
