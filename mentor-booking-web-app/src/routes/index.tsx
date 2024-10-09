615cec360514c5cf4be949ca29eafd5ac9c685b4
DatVT36 <vuthanhdat181@gmail.com>
2024-10-09 13:55:01 +0700

    feat: implement again login, logout with JWT [DatVT]

--- mentor-booking-web-app/src/routes/index.tsx
+++ mentor-booking-web-app/src/routes/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import paths from "./path";
 
 // Import your components/pages here
 import HomePage from "../pages/home/HomePage";
 
 import LoginPage from "../components/Auth/Login";
-import Register from "../components/Auth/Register"
+import Register from "../components/Auth/Register";
 import { FeedbackPage } from "../pages/meeting/FeedbackPage";
 import { StudentPage } from "../pages/user/student/StudentPage";
+import Dashboard from "../pages/admin/DashBoard";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -24,12 +21,12 @@ const AppRoutes: React.FC = () => {
         <Route path={paths.login} element={<LoginPage />} />
         <Route path={paths.register} element={<Register />} />
         <Route path={paths.feedback} element={<FeedbackPage />} />
-        <Route path={paths.stduent} element={<StudentPage />} />
-
+        <Route path={paths.student} element={<StudentPage />} />
+        <Route path={paths.student} element={<StudentPage />} />
+        <Route path={paths.dashboard} element={<Dashboard />} />
         {/* Add more routes here */}
       </Routes>
     </div>
-      
   );
 };
 
