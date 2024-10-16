d4b107d49367df5cec5dad61f7d163d9cb71d093
ThoNT16 <truongthonguyen107@gmail.com>
2024-10-16 13:04:59 +0700

    fix: change type name [ThoNT16]

--- mentor-booking-web-app/src/services/studentService.ts
+++ mentor-booking-web-app/src/services/studentService.ts
@@ -1,4 +1,4 @@
-import { PaginationModel, ResponseModel } from "../types/common.types";
+import { PaginationModel, ResponseRequestModel } from "../types/common.types";
 import { StudentType } from "../types/user.types";
 import { GET_ALL_STUDENTS } from "../utils/apiUrl/baseUrl";
 import axiosInstance from "../utils/axios/axiosInstance"
@@ -7,7 +7,7 @@ import axiosInstance from "../utils/axios/axiosInstance"
 
 const getAllStudent = async (page: string, size: string) => {
     const url = GET_ALL_STUDENTS
-    const result = (await axiosInstance.get<ResponseModel<PaginationModel<StudentType>>>(url.replace('{page}',page).replace('{size}',size)))
+    const result = (await axiosInstance.get<ResponseRequestModel<PaginationModel<StudentType>>>(url.replace('{page}',page).replace('{size}',size)))
     return result.data.responseRequestModel
 }
 
