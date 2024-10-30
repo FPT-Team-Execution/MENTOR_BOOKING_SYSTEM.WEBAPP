12a038be4f8bdea3c1324b0c521f65700e87a76d
ThoNT16 <truongthonguyen107@gmail.com>
2024-10-30 14:14:46 +0700

    feat: hide ok and cancel button of booking modal [thont]

--- mentor-booking-web-app/src/components/project/ProjectCard.tsx
+++ mentor-booking-web-app/src/components/project/ProjectCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import { Card, Tag } from "antd";
 import { CalendarOutlined, UserOutlined } from "@ant-design/icons";
