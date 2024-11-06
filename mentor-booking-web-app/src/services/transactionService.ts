e0517691c824abd752983d56578da17be240397b
ThoNT16 <truongthonguyen107@gmail.com>
2024-11-06 22:33:45 +0700

    feat: add history student transaction [thont]

--- mentor-booking-web-app/src/services/transactionService.ts
+++ mentor-booking-web-app/src/services/transactionService.ts
@@ -1,11 +1,18 @@
 import axiosInstance from "../utils/axios/axiosInstance"
 
 const getTransaction = async (page: number, pageSize: number) => {
-    const url = 'point-transactions?page={page}&size={size}'.replace("{page}",page.toString()).replace("{size}",pageSize.toString())
+    const url = '/point-transactions?page={page}&size={size}'.replace("{page}",page.toString()).replace("{size}",pageSize.toString())
+    const result = await axiosInstance.get(url)
+    return result.data
+}
+
+const getTransactionByStudentId = async (id: string,page: number, pageSize: number) => {
+    const url = '/point-transactions/{id}?page={page}&size={size}'.replace("{page}",page.toString()).replace("{size}",pageSize.toString()).replace("{id}",id)
     const result = await axiosInstance.get(url)
     return result.data
 }
 
 export const transactionService = {
-    getTransaction
+    getTransaction,
+    getTransactionByStudentId
 }
