8db73ecd25ad3efd8990773d1ef24dc0943c9970
NguyenDat0809 <datngx.dev@gmail.com>
2024-11-07 16:38:27 +0700

    temperary push

--- mentor-booking-web-app/src/utils/apiUrl/baseUrl.ts
+++ mentor-booking-web-app/src/utils/apiUrl/baseUrl.ts
@@ -12,41 +12,51 @@ export const MENTOR_REGISTER = `${BASE_URL}/auth/mentor/sign-up`;
 export const GOOGLE_SIGNIN = `${BASE_URL}/auth/google/signin`;
 export const CREATE_PROJECT = `${BASE_URL}/projects`;
 
-export const GET_ALL_STUDENTS = '/students?page={page}&size={size}&sortOrder=asc'
+export const GET_ALL_STUDENTS =
+  "/students?page={page}&size={size}&sortOrder=asc";
 
-export const REFRESH_TOKEN = '/auth/refresh'
-export const CALLBACK_URL = 'http://localhost:5173/auth/callback'
+export const REFRESH_TOKEN = "/auth/refresh";
+export const CALLBACK_URL = "http://localhost:5173/auth/callback";
 
 // Calendar Events URLs
-export const GET_EVENTS_BY_MENTOR_URL = (mentorId: string, page: number, size: number) =>
-    `${BASE_URL}/calendar-event/mentor/${mentorId}?page=${page}&size=${size}`;
+export const GET_EVENTS_BY_MENTOR_URL = (
+  mentorId: string,
+  page: number,
+  size: number
+) => `${BASE_URL}/calendar-event/mentor/${mentorId}?page=${page}&size=${size}`;
 
 export const GET_EVENT_BY_ID_URL = (calendarEventId: string) =>
-    `${BASE_URL}/calendar-event/${calendarEventId}`;
+  `${BASE_URL}/calendar-event/${calendarEventId}`;
 
 export const CREATE_EVENT_URL = `${BASE_URL}/calendar-event`;
 export const UPDATE_EVENT_URL = (calendarEventId: string) =>
-    `${BASE_URL}/calendar-event/${calendarEventId}`;
+  `${BASE_URL}/calendar-event/${calendarEventId}`;
 
 export const DELETE_EVENT_URL = (calendarEventId: string) =>
-    `${BASE_URL}/calendar-event/${calendarEventId}`;
+  `${BASE_URL}/calendar-event/${calendarEventId}`;
 
-export const GET_PROJECT_BY_ID = '/groups/students/{id}'
+export const GET_PROJECT_BY_ID = "/groups/students/{id}";
 
-export const GROUP = '/groups'
+export const GROUP = "/groups";
 
-export const SEARCH_STUDENT = '/groups/students/search/{searchItem}'
+export const SEARCH_STUDENT = "/groups/students/search/{searchItem}";
 
-export const SEARCH_MENTOR = '/mentors/search/{searchItem}'
-export const GET_MENTOR = '/mentors/{id}'
+export const SEARCH_MENTOR = "/mentors/search/{searchItem}";
+export const GET_MENTOR = "/mentors/{id}";
 
-export const GET_MENTORS = (page : number, size: number) => `/mentors?page=${page}&size=${size}`
-export const UPDATE_MENTOR =  '/mentors/profile'
-export const GET_MENTOR_DEGREES = (mentorId: string, page : number, size: number) => `/mentors/${mentorId}/degrees?page=${page}&size=${size}`
+export const GET_MENTORS = (page: number, size: number) =>
+  `/mentors?page=${page}&size=${size}`;
+export const UPDATE_MENTOR = "/mentors/profile";
+export const GET_MENTOR_DEGREES = (
+  mentorId: string,
+  page: number,
+  size: number
+) => `/mentors/${mentorId}/degrees?page=${page}&size=${size}`;
 
-export const REQUEST = '/requests'
+export const REQUEST = "/requests";
 
-export const GET_BUSY_TIMES = '/calendar-events/busy-event?MentorId={mentorId}&Day={day}'
+export const GET_BUSY_TIMES =
+  "/calendar-events/busy-event?MentorId={mentorId}&Day={day}";
 
 // Request URLs
 export const REQUEST_URL = `${BASE_URL}/requests`;
@@ -54,12 +64,12 @@ export const REQUEST_URL = `${BASE_URL}/requests`;
 export const GET_PROJECT_BY_STUDENT_ID = "/api/projects";
 
 export const SIGN_IN_GOOGLE_URL = (code: string) => {
-    return `https://localhost:7554/api/auth/signin-google?code=${code}&callbackuri=http://localhost:5173/auth/callback`
-}
+  return `https://localhost:7554/api/auth/signin-google?code=${code}&callbackuri=http://localhost:5173/auth/callback`;
+};
 
 export const GOOGLE_CALENDAR_FRAME_URL = (email: string | undefined) => {
-    return `https://calendar.google.com/calendar/embed?src=${email}&ctz=Asia/Ho_Chi_Minh&showTz=0`
-}
+  return `https://calendar.google.com/calendar/embed?src=${email}&ctz=Asia/Ho_Chi_Minh&showTz=0`;
+};
 
 export const MENTOR_OWN_PROFILE_URL = "/mentors/profile";
 
@@ -67,54 +77,70 @@ export const STUDENT_OWN_PROFILE_URL = "/students/profile";
 
 export const UPLOAD_AVATAR_URL = "/auth/avatar";
 
-export const MAJOR_API_URL = (id: string | undefined, query: PageRequestModel | undefined) => {
-    //get pagination
-    if (query) {
-        return `/majors?page=${query.page}&size=${query.size}`
-    }
-    //get by Id + delete
-    if (id) {
-        return `/majors/${id}`
-    }
-    //post + put
-    return `/majors`;
-}
+export const MAJOR_API_URL = (
+  id: string | undefined,
+  query: PageRequestModel | undefined
+) => {
+  //get pagination
+  if (query) {
+    return `/majors?page=${query.page}&size=${query.size}`;
+  }
+  //get by Id + delete
+  if (id) {
+    return `/majors/${id}`;
+  }
+  //post + put
+  return `/majors`;
+};
 
-export const PROJECT_API_URL = (id: string | undefined, query: PageRequestModel | undefined) => {
-    //get pagination
-    if (query) {
-        return `/projects?page=${query.page}&size=${query.size}&sortOrder=${query.sort}`
-    }
-    //get by Id + delete
-    if (id) {
-        return `/projects/${id}`
-    }
-    //post + put
-    return `/projects`;
-}
+export const PROJECT_API_URL = (
+  id: string | undefined,
+  query: PageRequestModel | undefined
+) => {
+  //get pagination
+  if (query) {
+    return `/projects?page=${query.page}&size=${query.size}&sortOrder=${query.sort}`;
+  }
+  //get by Id + delete
+  if (id) {
+    return `/projects/${id}`;
+  }
+  //post + put
+  return `/projects`;
+};
 
-export const SKILL_API_URL = (id: string | undefined, query: PageRequestModel | undefined) => {
-    //get pagination
-    if (query) {
-        return `/skills?page=${query.page}&size=${query.size}`
-    }
-    //get by Id + delete
-    if (id) {
-        return `/skills/${id}`
-    }
-    //post + put
-    return `/skills`;
-}
-export const STUDENT_API_URL = (id: string | undefined, query: PageRequestModel | undefined) => {
-    //get pagination
-    if (query) {
-        return `/students?page=${query.page}&size=${query.size}`
-    }
-    //get by Id + delete
-    if (id) {
-        return `/majors/${id}`
-    }
-    //post + put
-    return `/majors`;
-}
+export const SKILL_API_URL = (
+  id: string | undefined,
+  query: PageRequestModel | undefined
+) => {
+  //get pagination
+  if (query) {
+    return `/skills?page=${query.page}&size=${query.size}`;
+  }
+  //get by Id + delete
+  if (id) {
+    return `/skills/${id}`;
+  }
+  //post + put
+  return `/skills`;
+};
+export const STUDENT_API_URL = (
+  id: string | undefined,
+  query: PageRequestModel | undefined
+) => {
+  //get pagination
+  if (query) {
+    return `/students?page=${query.page}&size=${query.size}`;
+  }
+  //get by Id + delete
+  if (id) {
+    return `/majors/${id}`;
+  }
+  //post + put
+  return `/majors`;
+};
 
+export const getProgressesByProjectIdUrl = (
+  projectId: string | undefined,
+  query: PageRequestModel
+) => `/progresses/project/${projectId}?page=${query.page}&size=${query.size}`;
